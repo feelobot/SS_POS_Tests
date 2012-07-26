fbea01036e3436b4d4bcb29636db8ea76ecd6aa8
feelobot <felix@feelobot.com>
2012-07-26 07:31:53 +0400

    [update] installed headless feature for chrome

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,6 +4,18 @@ require 'selenium-webdriver'
 require 'capybara/cucumber'
 Capybara.default_driver = :selenium
 require 'timeout'
+require 'headless'
+headless = Headless.new
+at_exit do
+  headless.destroy
+end
 
+Before("@selenium,@javascript", "~@no-headless") do
+  headless.start if Capybara.current_driver == :selenium
+end
+
+After("@selenium,@javascript", "~@no-headless") do
+  headless.stop if Capybara.current_driver == :selenium
+end
 	
 
